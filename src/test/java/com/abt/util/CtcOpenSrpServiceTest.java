0ecbe99e4be0042245a3f86d3b868a64f7e33820
cozej4 <ilakozejumanne@gmail.com>
2026-03-22 11:52:16 +0300

    feat: add env flag for ltf event v2 flow

--- src/test/java/com/abt/util/CtcOpenSrpServiceTest.java
+++ src/test/java/com/abt/util/CtcOpenSrpServiceTest.java
@@ -1,10 +1,22 @@
 package com.abt.util;
 
+import com.abt.domain.Client;
+import com.abt.domain.LtfClientRequest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CtcOpenSrpServiceTest {
+
+    private static final String USE_LTF_EVENTS_VERSION_2_ENV_KEY = "USE_LTF_EVENTS_VERSION_2";
+
+    @AfterEach
+    void clearProperties() {
+        System.clearProperty(USE_LTF_EVENTS_VERSION_2_ENV_KEY);
+    }
 
     @Test
     void eventAddUrl_shouldBuildExpectedEventEndpoint() {
@@ -28,5 +40,56 @@ class CtcOpenSrpServiceTest {
                 "http://localhost:8080/opensrp/uniqueids/get?source=2&numberToGenerate=3",
                 CtcOpenSrpService.uniqueIdsUrl("http://localhost:8080/", 3)
         );
+    }
+
+    @Test
+    void isLtfEventVersion2Enabled_shouldDefaultToFalse() {
+        assertFalse(CtcOpenSrpService.isLtfEventVersion2Enabled());
+        assertTrue(CtcOpenSrpService.shouldGenerateAndSendLtfTask());
+    }
+
+    @Test
+    void isLtfEventVersion2Enabled_shouldUseConfiguredFlag() {
+        System.setProperty(USE_LTF_EVENTS_VERSION_2_ENV_KEY, "true");
+
+        assertTrue(CtcOpenSrpService.isLtfEventVersion2Enabled());
+        assertFalse(CtcOpenSrpService.shouldGenerateAndSendLtfTask());
+    }
+
+    @Test
+    void resolveLtfEvent_shouldUseCurrentImplementationWhenFlagDisabled() {
+        System.setProperty(USE_LTF_EVENTS_VERSION_2_ENV_KEY, "false");
+
+        assertEquals(
+                "Referral Registration",
+                CtcOpenSrpService.resolveLtfEvent(testClient(), testLtfRequest()).getEventType()
+        );
+    }
+
+    @Test
+    void resolveLtfEvent_shouldUseVersion2ImplementationWhenFlagEnabled() {
+        System.setProperty(USE_LTF_EVENTS_VERSION_2_ENV_KEY, "true");
+
+        assertEquals(
+                "LTF Referral Registration",
+                CtcOpenSrpService.resolveLtfEvent(testClient(), testLtfRequest()).getEventType()
+        );
+    }
+
+    private static Client testClient() {
+        return new Client("base-entity-id");
+    }
+
+    private static LtfClientRequest testLtfRequest() {
+        LtfClientRequest request = new LtfClientRequest();
+        request.setBaseEntityId("base-entity-id");
+        request.setUniqueId("unique-id");
+        request.setLocationId("location-id");
+        request.setProviderId("provider-id");
+        request.setTeamId("team-id");
+        request.setTeam("team");
+        request.setRecGuid("rec-guid");
+        request.setLastAppointmentDate("2026-03-20");
+        return request;
     }
 }
